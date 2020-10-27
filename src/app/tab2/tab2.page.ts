976eb0d6cddfb90c31326fcb019cc27447b87192
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2020-10-26 23:10:20 -0400

    Delete photos tab2 and style the naruto tab and change the icons pon di tabs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
-
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -12,9 +12,31 @@ export class Tab2Page {
 
   // constructor() {}
   // from tut
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
   async ngOnInit() {
@@ -22,3 +44,5 @@ export class Tab2Page {
   }
 
 }
+
+// ng config -g cli.warnings.versionMismatch false
